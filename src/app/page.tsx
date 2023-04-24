421f6f7c56f9be6f5068ac64a6ec38a0acd19a29
Pablo George <pablogeokar@hotmail.com>
2023-04-24 10:36:20 -0300

    First commit, NextJS 13 clean installation

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-cyan-600">Dashboard</h1>
+      <span className="text-cyan-100">NextJS 13</span>
+    </div>
+  );
+}
